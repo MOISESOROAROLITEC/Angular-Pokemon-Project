e33fb5cd9cb8e771ffdc19dec57f1c12bb8330ff
MOISESOROAROLITEC <moisesoro@arolitec.com>
2023-05-29 15:19:19 +0000

    add methode to get pokemon types and get pokemon by id

--- src/app/pokemon/pokemon.service.ts
+++ src/app/pokemon/pokemon.service.ts
@@ -6,10 +6,32 @@ import { POKEMONS } from './mock-pokemons';
 	providedIn: 'root'
 })
 export class PokemonService {
+	typesList: string[];
+
+	constructor() { }
 
 	getPokemonList(): Pokemon[] {
 		return POKEMONS
 	}
 
+	getPokemonById(pokemonId: number): Pokemon | undefined {
+		return POKEMONS.find(pokemon => pokemon.id == pokemonId)
+	}
+
+	getPokemonTypesList(): string[] {
+
+		POKEMONS.map(pokemon => {
+
+			pokemon.types.map(type => {
+
+				if (!this.typesList.includes(type)) {
+					this.typesList = [...this.typesList, type]
+				};
+
+			});
+
+		});
+		return this.typesList;
+	}
 
 }
