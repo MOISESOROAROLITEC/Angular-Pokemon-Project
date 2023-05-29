efb6fbb2d8c0c4b56510416045edd62bae06d54c
MOISESOROAROLITEC <moisesoro@arolitec.com>
2023-05-29 11:14:06 +0000

    create pokemon module and adding all component relative of pokemon

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
 import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
-import { EditPokemonComponent } from './edit-pokemon/edit-pokemon.component';
+import { EditPokemonComponent } from './pokemon/edit-pokemon/edit-pokemon.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,14 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 
 import { CardModule } from 'primeng/card';
-import { HoverCardDirective } from './hover-card.directive';
-import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
+import { HoverCardDirective } from './pokemon/hover-card.directive';
+import { PokemonTypeColorPipe } from './pokemon/pokemon-type-color.pipe';
 import { ChipModule } from 'primeng/chip';
 import { ButtonModule } from 'primeng/button';
-import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
-import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
-import { EditPokemonComponent } from './edit-pokemon/edit-pokemon.component';
+import { ListPokemonComponent } from './pokemon/list-pokemon/list-pokemon.component';
+import { EditPokemonComponent } from './pokemon/edit-pokemon/edit-pokemon.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { DetailPokemonComponent } from './pokemon/detail-pokemon/detail-pokemon.component';
 
 @NgModule({
 	declarations: [
@@ -23,8 +23,8 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 		PokemonTypeColorPipe,
 		ListPokemonComponent,
 		DetailPokemonComponent,
-  EditPokemonComponent,
-  PageNotFoundComponent
+		EditPokemonComponent,
+		PageNotFoundComponent
 	],
 	imports: [
 		BrowserModule,
