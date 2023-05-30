64342e6affb2072cb6517178b03803f035ac2ec9
MOISESOROAROLITEC <moisesoro@arolitec.com>
2023-05-30 10:14:21 +0000

    partition of style, html and ts files

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -3,40 +3,8 @@ import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-page-not-found',
-	template: `
-		<div class="page-not-found">
-			<div class="status-code">
-				<h1>
-					404
-				</h1>
-				<img class="not-found-image" src="../../assets/pokemon_images/293.png">
-			</div>
-			<div>
-				<h3>
-					Cette page est introuvable
-				</h3>
-			</div>
-			<p-button label="Retour à la liste des pokemons" icon="pi pi-angle-left" class="return-btn" styleClass="p-button-text p-button-primary"
-					(click)="returnButton()"></p-button>
-	</div>
-  `,
-	styles: [`
-		.page-not-found {
-			 text-align: center; 
-			}
-		.status-code h1{
-			font-size: 4rem;
-			color: red;
-			color: red;
-			font-weight: bold;
-			letter-spacing: 1.5rem;
-		}
-		.not-found-image{
-			max-height: 50vh;
-			max-width: 90vw;
-			animation: anim-pok-image 0.8s linear infinite alternate both;
-		}
-	`]
+	templateUrl: './page-not-found.component.html',
+	styleUrls: ['./page-not-found.component.scss']
 })
 export class PageNotFoundComponent {
 	constructor(private route: Router) { }
