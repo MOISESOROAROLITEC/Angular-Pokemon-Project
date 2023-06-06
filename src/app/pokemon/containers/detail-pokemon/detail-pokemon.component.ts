e9291c37021c437633edd36175b957041f6ae847
moisesoroarolitec <moisesoro@arolitec.com>
2023-06-06 21:08:25 +0000

    add toast when pokemon is deleted

--- src/app/pokemon/containers/detail-pokemon/detail-pokemon.component.ts
+++ src/app/pokemon/containers/detail-pokemon/detail-pokemon.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Pokemon } from '../../models/pokemon.model';
 import { PokemonService } from '../../pokemon.service';
+import { NotificationService } from 'src/app/notification.service';
 
 @Component({
 	selector: 'app-detail-pokemon',
@@ -14,7 +15,8 @@ export class DetailPokemonComponent implements OnInit {
 	constructor(
 		private activatedRoute: ActivatedRoute,
 		private route: Router,
-		private pokemonService: PokemonService
+		private pokemonService: PokemonService,
+		private notifService: NotificationService
 	) { }
 
 	ngOnInit() {
@@ -41,7 +43,7 @@ export class DetailPokemonComponent implements OnInit {
 	deletePokemon(id: number) {
 		this.pokemonService.deletePokemon(id).subscribe(
 			() => {
-
+				this.notifService.showSuccess("Pokémon supprimé avec succèss");
 				this.gotToPokemonsList()
 			}
 		);
