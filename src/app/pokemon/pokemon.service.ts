12679ad0b69959cf344ac3781c70f0ba642e1a3f
moisesoroarolitec <moisesoro@arolitec.com>
2023-06-09 16:08:37 +0000

    custom pokemon detail message

--- src/app/pokemon/pokemon.service.ts
+++ src/app/pokemon/pokemon.service.ts
@@ -2,25 +2,35 @@ import { Injectable, OnInit } from '@angular/core';
 
 import { Pokemon } from './models/pokemon.model';
 import { POKEMONS } from './mock-pokemons';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Observable, catchError, of, tap } from 'rxjs';
+import { NotificationService } from '../notification.service';
 
 @Injectable()
 export class PokemonService {
 
 	constructor(
 		private http: HttpClient,
+		private notificationService: NotificationService
 	) { }
 
 	getPokemonList(): Observable<Pokemon[]> {
 		return this.http.get<Pokemon[]>('api/pokemons').pipe(
-			catchError((error: any, caught) => this.handleError(error, caught))
+			catchError((error: any, caught) => this.handleError(error, caught.subscribe()))
 		);
 	}
 
 	getPokemonById(pokemonId: number): Observable<Pokemon | undefined> {
 		return this.http.get<Pokemon>(`api/pokemons/${pokemonId}`).pipe(
-			catchError((error: Error, caught: any) => this.handleError(error, caught))
+			catchError((error: Error, caught: any) => {
+
+				let notificationMessage: string | undefined = undefined;
+
+				if ((error as HttpErrorResponse).status == 404) {
+					notificationMessage = "Ce pokémon est introuvable"
+				}
+				return this.handleError(error, notificationMessage)
+			})
 		);
 	}
 
@@ -57,7 +67,9 @@ export class PokemonService {
 		)
 	}
 
-	private handleError(error: Error, errorValue: any = undefined) {
+	private handleError(error: Error, errorValue: any = "Une erreur inconnue s'est produite !") {
+
+		this.notificationService.showError(errorValue)
 		return of(errorValue);
 	}
 
